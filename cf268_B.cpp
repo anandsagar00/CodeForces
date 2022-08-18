2304bb9800c4543e64b3b9ae0924a16ac084882c
anandsagar00 <80447047+anandsagar00@users.noreply.github.com>
2022-08-19 00:55:54 +0530

    Update cf268_B.cpp

--- cf268_B.cpp
+++ cf268_B.cpp
@@ -3,18 +3,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     //we observe a pattern to be formed 
+//     long long int ans=n;
+//     int i=2,j=1;
+//     while(j<n)
+//     {
+//         ans+=(((n-j)*i)-j);
+//         j++;
+//         i++;
+//     }
+//     cout<<ans<<"\n";
+// }
+
 int main()
 {
     int n;
     cin>>n;
     //we observe a pattern to be formed 
-    long long int ans=n;
-    int i=2,j=1;
-    while(j<n)
+    long long int ans=0;
+    //we will count the mistakes this time to guess correct button each time , and we will add n at the end as there would be 1 
+    // guess at every step right
+    int i=1;
+    while(i<=n)
     {
-        ans+=(((n-j)*i)-j);
-        j++;
+        ans=ans+((n-i)*i);
         i++;
     }
-    cout<<ans<<"\n";
+    cout<<ans+n<<"\n";
 }
